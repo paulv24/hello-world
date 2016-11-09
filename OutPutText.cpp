951439a7bb2eb1d449a491047a3992af88f9e60c
paulv24 <paul.k.vo@gmail.com>
2016-11-09 10:37:28 -0600

    Create OutPutText.cpp

---
+++ OutPutText.cpp
@@ -0,0 +1,10 @@
+//C++:Output text to screen
+#include <iostream>
+int main()
+{
+  std::cout < < "This is a simple program that prints out lines of text." < < std::endl;
+  std::cout < < "Replace with your text here" < < std::endl;
+  std::cout < < "and here too." < < std::endl;
+  return 0; // Return to the operating system
+}
+
